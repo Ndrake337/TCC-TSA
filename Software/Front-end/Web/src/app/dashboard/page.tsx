d8da0fb7f22fe4183d06571b4193c38230b94192
Matheus Felipe Neves Campos Cardoso <matheus.felipe337@gmail.com>
2023-10-17 00:03:07 -0300

    Mudanças no projeto web e criação do projeto mobile com expo

--- Software/Front-end/Web/src/app/dashboard/page.tsx
+++ Software/Front-end/Web/src/app/dashboard/page.tsx
@@ -1,3 +1,7 @@
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
 }
--- Software/Front-end/Web/src/app/page.tsx
+++ Software/Front-end/Web/src/app/page.tsx
@@ -5,11 +5,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col w-full h-fit items-center">
-      <div className="flex flex-col items-center gap-2">
+      <div className="flex flex-col items-center gap-5">
         <Image src={logo} className="w-1/5" alt="Logo" />
         <strong>Bem Vindo ao SISTEMA DE TRACKER SOLAR AUTOLIMPANTE,</strong>
         <span>Aqui vão as principais Informações do projeto</span>
-        <div className="flex flex-row gap-8">
+        <div className="flex flex-row gap-5">
           <InfoCard />
           <InfoCard />
           <InfoCard />
