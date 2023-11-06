5973986e61c05eedc9711d2e66edc87a811069fa
Matheus Felipe Neves Campos Cardoso <matheus.felipe337@gmail.com>
2023-11-06 15:54:26 -0300

    feat(contato): Create contact form structure

--- Software/Front-end/Web/src/app/contato/page.tsx
+++ Software/Front-end/Web/src/app/contato/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/Button";
+
 export default function Dashboard() {
-  return <h1>Contato</h1>;
+  return (
+    <form className="flex flex-col gap-3 w-full">
+      <input type="text" placeholder="Digite seu Nome" />
+      <input type="email" placeholder="Digite seu e-mail" />
+      <input type="textarea" placeholder="Mensagem" />
+      <Button type="submit" label="Enviar" />
+    </form>
+  );
 }
