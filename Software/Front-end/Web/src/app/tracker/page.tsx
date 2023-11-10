e65367a9112099bef3513f2347e4042b6078bb75
Matheus Felipe Neves Campos Cardoso <matheus.felipe337@gmail.com>
2023-11-10 14:50:26 -0300

    feat(tracker panels): adding tracker panels connection with back-end api, functions from delete and create a new panel implemented, on hold:function for update a existing panel

--- Software/Front-end/Web/src/app/tracker/page.tsx
+++ Software/Front-end/Web/src/app/tracker/page.tsx
@@ -1,12 +1,90 @@
+"use client";
 import { Button } from "@/components/Button";
+import EditRoundedIcon from "@mui/icons-material/EditRounded";
+import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
+import { useEffect, useState } from "react";
+import { iPanel, iPanels } from "@/interfaces/iPanels";
+import axios from "axios";
 
 export default function Dashboard() {
+  const [apiData, setApiData] = useState<iPanel[]>();
+  const fetchPanels = async () => {
+    const res = await fetch("http://localhost:3333/panels");
+    const data: iPanels = await res.json();
+
+    console.log(data.pannels);
+
+    setApiData(data.pannels);
+  };
+
+  useEffect(() => {
+    fetchPanels();
+  }, []);
+
+  function editPanel(id: string) {
+    console.log(`Edit Panel: ${id}`);
+  }
+
+  async function deletePanel(id: string) {
+    console.log(`Delete Panel: ${id}`);
+
+    await axios.delete(`http://localhost:3333/panels/${id}`);
+
+    fetchPanels();
+  }
+
+  async function createPanel(name: string) {
+    console.log(`Create Panel: ${name}`);
+
+    await axios.post("http://localhost:3333/panels", {
+      name,
+    });
+
+    fetchPanels();
+  }
+
+  function generateLines() {
+    const returnArray = apiData?.map((panel) => {
+      return (
+        <tr key={panel.id}>
+          <td>{panel.name}</td>
+          <td>{panel.id}</td>
+          <td className="flex flex-row gap-3">
+            <EditRoundedIcon
+              onClick={() => {
+                editPanel(panel.id);
+              }}
+            />
+            <span>|</span>{" "}
+            <DeleteRoundedIcon
+              onClick={() => {
+                deletePanel(panel.id);
+              }}
+            />
+          </td>
+        </tr>
+      );
+    });
+
+    return returnArray;
+  }
+
   return (
     <div className="flex flex-col w-full items-center gap-4">
       <strong>Cadastre e consulte painéis solares no sistema</strong>
-      <form className="flex flex-col gap-4 w-full">
+      <form
+        className="flex flex-col gap-4 w-full"
+        onSubmit={(event) => {
+          event.preventDefault();
+          createPanel(event.target.panelName.value);
+        }}
+      >
         <div className="flex flex-row max-lg:flex-col gap-1 w-full ">
-          <input type="Name" placeholder="Nome do Painel Solar" />
+          <input
+            name="panelName"
+            type="Name"
+            placeholder="Nome do Painel Solar"
+          />
         </div>
         <Button label="Cadastrar" type="submit" />
       </form>
@@ -20,18 +98,7 @@ export default function Dashboard() {
             <th>Options</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </tbody>
+        <tbody>{generateLines()}</tbody>
       </table>
     </div>
   );
---
+++ Software/Front-end/Web/src/interfaces/iPanels.d.ts
@@ -0,0 +1,8 @@
+export interface iPanel {
+  id: string;
+  name: string;
+}
+
+export interface iPanels {
+  pannels: iPanel[];
+}
