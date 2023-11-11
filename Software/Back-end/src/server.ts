c69bdebc11e7ce2f84668aa8a8cbfdfb6815af2c
Matheus Felipe Neves Campos Cardoso <matheus.felipe337@gmail.com>
2023-11-11 17:18:48 -0300

    fix(deploy): render error on find available port for application

--- Software/Back-end/src/server.ts
+++ Software/Back-end/src/server.ts
@@ -1,6 +1,11 @@
 import { app } from "./app";
 import { env } from "./env";
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log("HTTP Server Running");
-});
+app
+  .listen({
+    host: "RENDER" in process.env ? "0.0.0.0" : "localhost",
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log("HTTP Server Running");
+  });
