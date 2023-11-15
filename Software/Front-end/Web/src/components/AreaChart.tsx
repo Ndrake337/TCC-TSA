685161b90be2de968cb70454e3b297363e07701f
Matheus Felipe Neves Campos Cardoso <matheus.felipe337@gmail.com>
2023-11-15 16:06:48 -0300

    chore: fixing typescript errors for deploy

--- Software/Front-end/Web/src/components/AreaChart.tsx
+++ Software/Front-end/Web/src/components/AreaChart.tsx
@@ -1,7 +1,7 @@
 import { Line } from "react-chartjs-2";
 import "chart.js/auto";
 
-export function AreaChart({ chartData, chartTitle }) {
+export function AreaChart({ chartData, chartTitle }: any) {
   return (
     <div className="flex flex-col items-center p-6 shadow-lg rounded-lg">
       <strong className="text-xl">{chartTitle}</strong>
